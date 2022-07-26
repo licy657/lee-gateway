16d6de2bcb060abe0a0d29fa0d365e3b2c976685
lichangyi <562072775@qq.ocm>
2022-07-26 11:20:17 +0800

    update the code for init gateway route

--- src/main/java/com/lee/gateway/configuration/TokenValFilter.java
+++ src/main/java/com/lee/gateway/configuration/TokenValFilter.java
@@ -3,6 +3,7 @@ package com.lee.gateway.configuration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -24,11 +25,19 @@ public class TokenValFilter implements GlobalFilter, Ordered {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         URI uri = exchange.getRequest().getURI();
         log.info("请求的url：{}", uri);
-        return chain.filter(exchange);
+        Object attribute = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
+
+        long start = System.currentTimeMillis();
+
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            long end = System.currentTimeMillis();
+            log.info("实际调用地址为：{}，调用耗时为：{}ms", attribute, (end - start));
+        }));
     }
 
     @Override
     public int getOrder() {
-        return -1;
+        // 优先级设置最低，先让TokenValFilter被调用
+        return Ordered.LOWEST_PRECEDENCE;
     }
 }
